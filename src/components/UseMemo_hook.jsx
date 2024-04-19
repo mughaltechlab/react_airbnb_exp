335e94dc78b46fba0e69762f2e673364c1a25125
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-04-19 19:39:50 +0500

    Update UseMemo_hook.jsx

--- src/components/UseMemo_hook.jsx
+++ src/components/UseMemo_hook.jsx
@@ -5,6 +5,17 @@ function UseMemo_hook(){
     const [number,setNumber] = useState(0)
     const [dark,setDark] = useState(false)
 
+    const nums = new Array(30_000_000).fill(0).map((_,i)=>{
+        return {
+            index : i,
+            isMagical : i === 29_000_000 ? true : false,
+        }
+    })
+
+    const [arrayNum, setArrayNum] = useState(nums)
+
+    const magical = arrayNum.find(item=>item.isMagical === true)
+
     // * here we use useMemo to make app faster, this callback function only rerendered when its dependancy will be change
     const doubleNum = useMemo(()=>{
         return slowFunc(number) 
@@ -34,6 +45,9 @@ function UseMemo_hook(){
                 <div style={themeStyle}>
                     {doubleNum}
                 </div>
+                <div style={themeStyle}>
+                    {magical.index}
+                </div>
             </div>
             
 
@@ -41,7 +55,11 @@ function UseMemo_hook(){
     )
 }
 
+
+
 function slowFunc(num){
+
+    
     for(let i=0; i< 1000000000000000; i++){
         return num * 2
     }
